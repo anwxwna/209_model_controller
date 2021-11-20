1579e3e96258a72bb37e8d6950074817b96b5730
anwxwna <anwchatto@gmail.com>
2021-11-19 16:37:36 -0800

    rolling

---
+++ rolling_constraints_matrix.m
@@ -0,0 +1,8 @@
+function f = rolling_constraints_matrix(parameters)
+% alpha,beta,l,d
+alpha = parameters(1,1);
+beta = parameters(1,2);
+l = parameters(1,3);
+d= parameters(1,4);
+
+% rolling constraints
