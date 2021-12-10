e6aec0d4163cc7a35ae79ea64e51a63f866bd0cb
anwxwna <anwchatto@gmail.com>
2021-12-09 19:50:29 -0800

    additions

--- classify_components.m
+++ classify_components.m
@@ -1,5 +1,6 @@
-function [wheels_f,wheels_s,wmr_possible] = classify_components(WMR)
+function [wheels_f,wheels_s,wmr_possible,wmr_type] = classify_components(WMR)
 %% AXLE MAPS ==============================================================
+wmr_type = '';
 wheels_f =[];
 wheels_s =[];
 wheel_num = WMR.wheel_num ;
@@ -12,7 +13,8 @@ active = [];
 robot_x_axis = [1;0;0;0];
 robot_z_axis = [0;0;1;0];
 robot_y_axis = [0;1;0;0];
-% if else ladder approach to generate model================================
+
+% if else ladder approach to generate model
 % Check orientation of wheels add to an array 
 % Add euler angles of the wheels from the tform matrices into an array
 % Get directional vectors for all the wheels 
@@ -40,9 +42,10 @@ for i=1:wheel_num
             % Check if wheels on same axle add to array 
             % x angle of rotation is opposite and the wheel coordinate
             % center is same
+
             % Wheels are same axle -> z-axis is aligned 
             if (transXYZi(1) == transXYZj(1)) && (sign(transXYZi(2)) ~= sign(transXYZj(2))) && (transXYZi(3) == transXYZj(3)) && (eulZYXi(1) == 0) && (eulZYXi(2) == 0 ) && (eulZYXj(1) == 0) && (eulZYXj(2) == 0 )
-    %       Don't include duplicates 
+                % Don't include duplicates 
                 inv_map = strcat(int2str(j),'_',int2str(i));
                 if isKey(same_axle_map,inv_map) ~=1
                     mapping_name = strcat(int2str(i),'_',int2str(j));
@@ -81,7 +84,7 @@ for i=1:wheel_num
     wheel_center = tform*[0;0;0;1];
     wheel_centers = [wheel_centers;wheel_center.'];
 end
-% Actuators
+% Actuators =============================================================
 % actuator wrt ROBOT FRAME
 actsEUL = [];
 act_centers=[];
@@ -100,50 +103,129 @@ for i=keys(same_axle_map)
     w1 = W(1);
     w2 = W(2);
     axle_vec = wheel_centers(w1,1:4)- wheel_centers(w2,1:4);
-    axle_vectors = [axle_vectors;axle_vec];
+    axle_vectors = [axle_vectors;axle_vec];  % same axle wheels
+
 %   Relative position of actuators wrt all the axles
     for k=actuator_num
-%          axle_actuator_map_name = strcat(int2str(i),'_',int2str(k));
+%   Going through each actuator in loop 
          act_center = actuator_tforms(4*(k-1)+1:4*(k-1)+1+3,1:4) *robot_z_axis;
          angle = acos(min(1,max(-1, act_center(:).' * axle_vec(:) / norm(act_center) / norm(axle_vec) )));
-         % Actuator parallel to wheel axles
+         % Actuator parallel to wheel axles---------
+%          Adding the ACTIVE wheels to the active set (fixed wheels)
          if angle == 0
             if act_centers(k) == wheel_centers(w1,1:4)
                 active = [active, w1];
             elseif act_centers(k) == wheel_centers(w2,1:4)
                 active = [active, w2];
             end
-    % Actuator located orthogonal to wheel axles 
+%  Check if actuated wheels share same axes
+%  If steered wheels on same axes as fixed wheels, consider as fixed
+            % Actuator located orthogonal to wheel axles-------------------
+%           Center steerable wheels are the passive wheels with a hinged
+%           actuator orthogonal to it 
+%             This gives steering wheels that are co-aligned and oriented
+%             together fulfilling requirement that 
          elseif angle == pi/2
             steering = [steering,w1,w2];
          end
          act_axle_angles = [act_axle_angles;angle];
     end
-
-    
- 
-    %   Steering Wheels
-    %   Others (?)
-    % Passive Wheels 
-    % Check radius of wheels on the same axle
-    % Distance between wheels on the same axle
-    % Distance between wheels on parallel axles
 end
-%% ICR of wheels (?) [CHECK FOR SINGULARITIES]=============================
-icr = [];
+% Only passive wheel axles are considered steering wheels
+for i=1:length(steering)
+    if ismember(steering(i), active)
+        steering(i) = [];
+    end
 
 
 
+end
+%% [CHECK FOR SINGULARITIES]=============================
+icrs = [];
+% ICR of wheels
+for i=1:length(steering)
+    for j=2:length(steering)
+        if i ~= length(steering)
+            shift_i =i+1;
+        else
+            shift_i =1;
+        end
+        if j ~= length(steering)
+            shift_j =j+1;
+        else
+            shift_j =1;
+        end
+        
+        x = [wheel_centers(i,1:3); wheel_centers(shift_i,1:3)];
+        y = [wheel_centers(j,1:3); wheel_centers(shift_j,1:3)];
+        [intersection_point ,dist]= lineIntersect3D(x,y);
+        icrs = [icrs;intersection_point];
+    end
+end
+% This will set the wmr_possible variable for the particular robotic
+% configuration reference: https://ms.copernicus.org/articles/7/93/2016/ms-7-93-2016.pdf
+
+%  TYPE A --------------------------
+% wheel axes of all wheels of a robot with coaligned center-steerable wheels (HW-type Vb) coincide
+% ≥ 2 cs wheels , 0 fixed wheels
+if (length(steering) >=2) && (isempty(active))
+    for i=length(steering)
+        for j=length(steering)
+            if isKey(same_axle_map, strcat(int2str(i),'_',int2str(j)))
+                wmr_possible = 0;
+            end
+        end
+    end
+end
+
+%  TYPE B --------------------------
+% A singularity of type A is only present for robots with coaligned wheel 
+% contact points (HW-type Vb). When the wheel contact points are not on a
+% straight line (HW-type Va), then one or more (but not all) wheel axes may be coaligned.
+% ≥ 3 cs wheels , 0 fixed wheels
+if (length(steering) >=3) && (isempty(active))
+    for i=length(steering)
+        for j=length(steering)
+            if i ~=j
+                if isKey(same_axle_map, strcat(int2str(i),'_',int2str(j)))
+                    wmr_possible = 0;
+                end
+            end
+        end
+    end
+end
+    
+%  TYPE C --------------------------
+% A robot is in a type C singularity when the ICR coincides with the contact point of a center-steerable standard wheel
+if (length(steering) >=2) && (isempty(active))
+   for i= 1:length(icrs)
+       for j=1:length(steering)
+        if isequal(icrs(i),wheel_centers(steering(j),1:3)) ==1
+            wmr_possible =0;
+        end
+       end
+   end
+
+end
+% ACTIVE CHECK Check if there are active wheels that are not on the same axle -------------
+for i=1:length(active)
+    for j=1:length(active)
+        if (i~=j) && (i<j)
+            if isKey(same_axle_map, strcat(int2str(i),'_',int2str(j))) ~= 1
+                wmr_possible=0;
+            end
+        end
+    end
+end
 %% ANGLE calculations =====================================================
 % calculate alpha, beta and l for each wheel wrt P=========================
 % Use the tform matrix to get the coords wrt to the ROBOT frame from the
-
 % WHEEL frame
 wheels_alphas = [];
 wheels_betas = [];
 for i=1:wheel_num
     % Get vector of point from P to wheel center -> WP
-%     wheel_center= wheel_centers((3*(i-1))+1:(3*(i-1))+2,1);
+%   Wheel_center= wheel_centers((3*(i-1))+1:(3*(i-1))+2,1);
     wheel_center = wheel_centers(i,1:4);
     wheel_center(4) = 0;
     wheel_center= wheel_center.';
@@ -159,13 +241,41 @@ for i=1:wheel_num
     beta = acos(min(1,max(-1, wheel_center(:).' * wheel_z_vec(:) / norm(wheel_center) / norm(wheel_z_vec) )));
     wheels_betas = [wheels_betas, beta];
 end
-
+%% Classification==========================================================
+% After counting all the actuated wheels of differnt types 
+% if the number of fixed wheels is == 0
+if length(active) ==0
+%     If number of c-steerable wheels is 0
+    if length(steering) ==0
+        wmr_type = '(3,0)';
+    %     If number of c-steerable wheels is 1
+    elseif length(steering) == 1
+        wmr_type = '(2,1)';
+    %     If number of c-steerable wheels is >=3
+    elseif length(steering) >=3
+        wmr_type = '(1,2)';
+    %     If number of c-steerable wheels is >=2
+    elseif length(steering) >=2
+        wmr_type = '(2,0)';
+        wmr_type = '(1,2)';
+    end
+    
+end
+% if number of fixed wheels is > =1
+%     If number of c-steerable wheels is 0
+%     If number of c-steerable wheels is >=1
+if length(active) >=1
+    if length(steering) ==0
+        wmr_type = '(2,0)';
+    elseif length(steering) >=1
+            wmr_type = '(1,1)';
+    end
+end
 % These should be output of the above lines
-fixed = [1,2];
-% steering = [];
 
 for i=fixed
     wheels_f = [wheels_f; wheels_alphas(i),wheels_betas(i),norm(wheel_centers(i))];
 end
 wmr_possible = 1;
-end
+        
+        
